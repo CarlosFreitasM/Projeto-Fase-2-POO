934933bad0b6c78cd0b755e9d1822bde6a417372
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2022-12-27 16:27:40 +0000

    Remove comment

--- src/Rand.java
+++ src/Rand.java
@@ -4,16 +4,6 @@ import java.lang.Math;
 
 public final class Rand {
 
-  /*
-  public static int getRandomNumberInRange(int min, int max) {
-    if (min >= max) {
-      throw new IllegalArgumentException("max must be greater than min");
-    }
-
-    Random r = new Random();
-    return r.nextInt((max - min) + 1) + min;
-  }*/
-
   public static final int getRandomNumberInRange(int min, int max) {
     if (min >= max) {
       throw new IllegalArgumentException("max must be greater than min");
